ca1d04ad4f6c2c0fc988322f937131872c31b943
Abigail Chelanagt <abigailchelah@gmail.com>
2025-03-18 13:26:34 -0700

    Add index.js file

---
+++ index.js
@@ -0,0 +1,19 @@
+//  Scooter Constructor Function
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+//  Driver Constructor Function
+function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+//  PickupLocation Constructor Function
+function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+}
